10a934a511c257e70717b442fcffdf2dd7fc56e8
chaitanyaraje thorbole <rootcodesop@gmail.com>
2026-03-17 15:33:31 +0530

    DAY 02 : Implemented custom user model with role system

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('ngo', 'NGO'),
+        ('volunteer', 'Volunteer'),
+        ('donor', 'Donor'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
