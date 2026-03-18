7f77f92f02e9d0164dda9527cbb5b54fd0990739
chaitanyaraje thorbole <rootcodesop@gmail.com>
2026-03-18 13:44:26 +0530

    DAY 03 : Implemented user registration API using DRF

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import UserRegisterSerializer
 
-# Create your views here.
+
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'apps.users',
     'apps.cms',
     'apps.donations',
